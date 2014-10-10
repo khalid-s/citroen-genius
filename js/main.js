f404a6ccd7748dd9934a380fa0b176d23c6690ee
unknown <khalidsookia@gmail.com>
2014-10-10 12:05:21 +0200

    Added slider and nav hover effect

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
+jQuery(document).ready(function() {
 // Toggle the account's options 
-$( "#accnt" ).click(function() {
-    $( ".accnt-option" ).stop().toggle(150);
-});
-
+    $( "#accnt" ).click(function() {
+        $( ".accnt-option" ).stop().toggle(150);
+    });
 // ------------ End -----------------------
 
 // Move back up the page button
-jQuery(document).ready(function() {
     var offset = 220;
     var duration = 500;
     jQuery(window).scroll(function() {
@@ -21,4 +20,27 @@ jQuery(document).ready(function() {
         jQuery('html, body').animate({scrollTop: 0}, duration);
         return false;
     })
+// --------------- End move up page -----------------
+
+// slider init and options
+    $('.banner').unslider({
+        speed: 500,
+        delay: 3000,
+        complete: function() {},
+        keys: true,
+        dots: true,
+        fluid: false
+    });
+// --------------- End move up page -----------------
+
+// nav icons hover and active. Rename the src by adding -active to the png
+        $("nav ul li")
+            .mouseover(function() { 
+                var src = $(this).find('img').attr("src").replace(".png", "-active.png");
+                $(this).find('img').attr("src", src);
+            }).stop()
+            .mouseout(function() {
+                var src = $(this).find('img').attr("src").replace("-active.png", ".png");
+                $(this).find('img').attr("src", src);
+            }).stop();
 });
