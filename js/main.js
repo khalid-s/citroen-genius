99f659e58aee683948540188e95c3f230180bea3
unknown <khalidsookia@gmail.com>
2014-10-13 19:29:06 +0200

    added jquery calendar and scroll bar for home page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// init calendars in case there are many
 jQuery(document).ready(function() {
 // Toggle the account's options 
     $( "#accnt" ).hover(function() {
@@ -53,4 +54,97 @@ jQuery(document).ready(function() {
             $('ul', this).removeClass('active');
         }).stop();
 
+// --------------- End nav sub menus -----------------
+
+// styled scroll bar
+    $("#home .content .left .scrollable form").mCustomScrollbar({
+        theme:"minimal-dark",
+        alwaysShowScrollbar : 1
+    }).removeClass('mCS-autoHide');
+    
+// --------------- End styled scroll bar -----------------
+
+// calendar on home page
+
+    var calendars = {};
+
+    var thisMonth = moment().format('YYYY-MM');
+
+
+    var eventArray = [
+        { startDate: thisMonth + '-17', endDate: thisMonth + '-17', title: 'Another Multi-Day Event' },
+        { startDate: thisMonth + '-29', endDate: thisMonth + '-31', title: 'Another Multi-Day Event' },
+        { startDate: '2015-03-01', endDate: '2015-03-01', title: 'Another Multi-Day Event' },
+        { startDate: '2015-03-31', endDate: '2015-03-31', title: 'Another Multi-Day Event' }
+    ];
+      // the order of the click handlers is predictable.
+      // direct click action callbacks come first: click, nextMonth, previousMonth, nextYear, previousYear, or today.
+      // then onMonthChange (if the month changed).
+      // finally onYearChange (if the year changed).
+
+    calendars.clndr = $('.cal').clndr({
+        daysOfTheWeek: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
+        // set weekOffset to 0 to begin the calendar on sunday instead of monday
+        weekOffset: 1,
+        template: $('#template-calendar').html(),
+        events: eventArray,
+        // constraints: {
+        //   startDate: '2013-11-01',
+        //   endDate: '2013-11-15'
+        // },
+        clickEvents: {
+            click: function(target) {
+                console.log(target);
+                // if you turn the `constraints` option on, try this out:
+                // if($(target.element).hasClass('inactive')) {
+                //   console.log('not a valid datepicker date.');
+                // } else {
+                //   console.log('VALID datepicker date.');
+                // }
+            },
+            nextMonth: function() {
+            console.log('next month.');
+            },
+            previousMonth: function() {
+            console.log('previous month.');
+            },
+            onMonthChange: function() {
+            console.log('month changed.');
+            },
+            nextYear: function() {
+            console.log('next year.');
+            },
+            previousYear: function() {
+            console.log('previous year.');
+            },
+            onYearChange: function() {
+            console.log('year changed.');
+            }
+        },
+        multiDayEvents: {
+            startDate: 'startDate',
+            endDate: 'endDate'
+        },
+        showAdjacentMonths: true,
+        adjacentDaysChangeMonth: false
+    });
+
+      // bind both clndrs to the left and right arrow keys
+    $(document).keydown( function(e) {
+        if(e.keyCode == 37) {
+            // left arrow
+            calendars.clndr.back();
+        }
+        if(e.keyCode == 39) {
+            // right arrow
+            calendars.clndr.forward();
+        }
+    });
+
+    // $('.clndr-previous-button').html('<img src="img/calendar-previous.png" alt="">');
+
+    // $('.clndr-next-button').html('<img src="img/calendar-next.png" alt="">');
+
+
+// --------------- End calendar -----------------
 });
